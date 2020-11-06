30a3376d53722f381b9f896ebbe6a155b475110e
David Harris <jdh.programmer@gmail.com>
2020-11-06 13:53:23 -0600

    Working nytimes api on script.js. you can view index.html and check the console. it is console logging 10 articles about pizza. you can change the value of variable 'article' at the top and it will search for that.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function searchNyTimesArticles() {
+let article = "pizza";
+
+searchNyTimesArticles(article);
+
+function searchNyTimesArticles(search) {
 
     // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
     // api-key: hKauiyFtFuXV18MsKZqs8HM1pOcwpbAD
-    var queryURL = "https:api.nytimes.com/svc/search/v2/articlesearch.json?q=" + article + "&api-key=hKauiyFtFuXV18MsKZqs8HM1pOcwpbAD;"
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=hKauiyFtFuXV18MsKZqs8HM1pOcwpbAD"
+
+    // "https:api.nytimes.com/svc/search/v2/articlesearch/" + article + "&api-key=hKauiyFtFuXV18MsKZqs8HM1pOcwpbAD;"
 
     $.ajax({
         url: queryURL,
@@ -10,29 +16,16 @@ function searchNyTimesArticles() {
     }).then(function(response) {
         console.log(response);
 
-
-        // let $container = $(`<div>
-        //   <p>artil Name: ${response.name}</p>
-        //   <img src="${response.thumb_url}">
-        //   <p># of Fans Tracking: ${response.tracker_count}</p>
-        //   <p>Upcoming Events: ${response.upcoming_event_count}</p>
-        //   <a href="${response.facebook_page_url}">${response.facebook_page_url}</a>
-        //   <br>
-        //   <a href="https://www.bandsintown.com/a/944">https://www.bandsintown.com/a/944</a>
-        //   </div>
-        //   `);
-
-        // $("#artist-div").append($container);
     })
 }
 
-// Event handler for user clicking the select-artist button
+// Event handler for user submitting search button
 $("#search").on("submit", function(event) {
     // Preventing the button from trying to submit the form
     event.preventDefault();
-    // Storing the artist name
-    var article = $("#artist-input").val().trim();
+    // Storing the article input
+    var article = $("#article-input").val().trim();
 
     // Running the searchBandsInTown function(passing in the artist as an argument)
-    searchBandsInTown(artist);
+    searchNyTimesArticles(article);
 });
